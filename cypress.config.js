826d3c3f59d9c7caa2187c582d251bf35c3416b9
Sidney Pimentel <sidneyspe@gmail.com>
2026-01-21 08:50:59 -0300

    update project

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,10 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://wlsf82-hacker-stories.web.app',
+    specPattern: [
+      'cypress/e2e/**/*.cy.js',
+      'cypress/api/**/*.cy.js',
+    ],
     env: {
       hideCredentials: true,
       requestMode: true,
---
+++ cypress/support/api.js
@@ -0,0 +1,7 @@
+// API support file
+// Import shared commands and configurations
+
+import '../resources/data.config';
+import './index';
+
+// API-specific commands can be added here
